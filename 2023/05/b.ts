e57a9a020564af0b4b7c507aa73d35ffe4af8414
Aivars Liepa <aivarsliepa1992@gmail.com>
2023-12-15 22:35:14 +0200

    2023 day 5 optimized

--- 2023/05/b.ts
+++ 2023/05/b.ts
@@ -42,23 +42,41 @@ readLines("./input.txt").then(input => {
 
   for (let i = 0; i < seedRanges.length; i++) {
     const [start, length] = seedRanges[i];
-    for (let j = start; j < start + length; j++) {
-      let current = j;
-      for (let k = 0; k < queue.length; k++) {
-        current = getNext(current, queue[k]);
-      }
-      if (current < lowest) lowest = current;
+    let currentRange = [{ start, end: start + length - 1 }];
+
+    for (let j = 0; j < queue.length; j++) {
+      currentRange = getNextRange(currentRange, queue[j]);
     }
+
+    currentRange.forEach(r => {
+      if (r.start < lowest) lowest = r.start;
+    });
   }
 
   console.log(lowest);
   console.log(performance.now() - start);
 });
 
-function getNext(source: number, mappings: Mapping[]): number {
-  const mapping = mappings.find(m => {
-    return m.source <= source && m.source + m.range - 1 >= source;
-  });
-  if (!mapping) return source;
-  return mapping.target + (source - mapping.source);
+function getNextRange(range: { start: number; end: number }[], mappings: Mapping[]): { start: number; end: number }[] {
+  let newRages = [];
+
+  for (let i = 0; i < range.length; i++) {
+    const { start, end } = range[i];
+
+    for (let mapping of mappings) {
+      if (mapping.source <= end && mapping.source + mapping.range - 1 >= start) {
+        // Calculate the intersection of the range with the mapping range
+        let intersectionStart = Math.max(start, mapping.source);
+        let intersectionEnd = Math.min(end, mapping.source + mapping.range - 1);
+
+        // Apply the mapping to the start and end of the intersection
+        newRages.push({
+          start: mapping.target + (intersectionStart - mapping.source),
+          end: mapping.target + (intersectionEnd - mapping.source),
+        });
+      }
+    }
+  }
+
+  return newRages;
 }
