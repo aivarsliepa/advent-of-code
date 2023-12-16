67af8266e6ef5b28118bd103ed40fe6295465331
Aivars Liepa <aivarsliepa1992@gmail.com>
2023-12-16 14:15:06 +0200

    2023 day 9 part 2

---
+++ 2023/09/b.ts
@@ -0,0 +1,40 @@
+import { readLines } from "../../utils-ts";
+
+readLines("./input.txt")
+  .then(input => {
+    const histories = input.map(line => line.split(" ").map(Number));
+    const predictions = histories.map(getPrediction);
+    return predictions.reduce((acc, curr) => acc + curr, 0);
+  })
+  .then(output => {
+    console.log(output);
+  });
+
+function getPrediction(history: number[]): number {
+  const list = [history];
+
+  while (true) {
+    const last = list[list.length - 1];
+    list.push([]);
+    let areAllZeroes = true;
+    for (let i = 0; i < last.length - 1; i++) {
+      const diff = last[i + 1] - last[i];
+      list[list.length - 1].push(diff);
+      if (diff !== 0) areAllZeroes = false;
+    }
+    if (areAllZeroes) break;
+  }
+
+  for (let i = list.length - 1; i >= 0; i--) {
+    if (i === list.length - 1) {
+      list[i].unshift(0);
+    } else {
+      const prevIndex = i + 1;
+      const prevLast = list[prevIndex][0];
+      const newValue = list[i][0] - prevLast;
+      list[i].unshift(newValue);
+    }
+  }
+
+  return list[0][0];
+}
