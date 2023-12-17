60bcfb819b4e0e19e5b9799f12abc0d60c01cf7c
Aivars Liepa <aivarsliepa1992@gmail.com>
2023-12-17 18:53:40 +0200

    fix memo, add debug

--- 2023/12/b.ts
+++ 2023/12/b.ts
@@ -1,8 +1,9 @@
 import { readLines } from "../../utils-ts";
 
 let memoHits = 0;
+let wrongCombinations = 0;
 
-readLines("./input.txt")
+readLines("./test-input.txt")
   .then(input => {
     return input
       .map((line, i) => {
@@ -12,9 +13,10 @@ readLines("./input.txt")
         const newString = new Array(5).fill(str).join("?");
         const groups = newGroupsStr.split(",").map(Number);
         console.log(newString, newGroupsStr);
+        wrongCombinations = 0;
         const result = countCombinations(newString, groups);
         const diff = performance.now() - start;
-        console.log(result, performance.now() - start, i, memoHits);
+        console.log(result, performance.now() - start, i, memoHits, wrongCombinations);
 
         return result;
       })
@@ -26,6 +28,7 @@ readLines("./input.txt")
 
 function countCombinations(str: string, groups: number[], strIndex = 0, groupIndex = 0, count = 0, memo: Record<string, number> = {}): number {
   if (groupIndex === groups.length && count > 0) {
+    wrongCombinations++;
     return 0;
   }
 
@@ -41,11 +44,12 @@ function countCombinations(str: string, groups: number[], strIndex = 0, groupInd
       return 1;
     }
 
+    wrongCombinations++;
     return 0;
   }
 
   const key = `${strIndex}-${groupIndex}-${count}`;
-  if (memo[key]) {
+  if (key in memo) {
     memoHits++;
     return memo[key];
   }
@@ -54,24 +58,30 @@ function countCombinations(str: string, groups: number[], strIndex = 0, groupInd
   const char = str[strIndex];
   const shouldBePound = count > 0 && count < currentGroupLength;
   const shouldBeDot = didFinishGroup;
-  if (shouldBeDot && char === "#") return 0;
-  if (shouldBePound && char === ".") return 0;
+  if (shouldBeDot && char === "#") {
+    wrongCombinations++;
+    return 0;
+  }
+  if (shouldBePound && char === ".") {
+    wrongCombinations++;
+    return 0;
+  }
 
   let total = 0;
   const nextStrIndex = strIndex + 1;
   if (char === "?") {
     if (!shouldBeDot) {
       // pound
-      total += countCombinations(str, groups, nextStrIndex, groupIndex, count + 1);
+      total += countCombinations(str, groups, nextStrIndex, groupIndex, count + 1, memo);
     }
     if (!shouldBePound) {
       // dot
-      total += countCombinations(str, groups, nextStrIndex, groupIndex, 0);
+      total += countCombinations(str, groups, nextStrIndex, groupIndex, 0, memo);
     }
   } else if (char === ".") {
-    total += countCombinations(str, groups, nextStrIndex, groupIndex, 0);
+    total += countCombinations(str, groups, nextStrIndex, groupIndex, 0, memo);
   } else if (char === "#") {
-    total += countCombinations(str, groups, nextStrIndex, groupIndex, count + 1);
+    total += countCombinations(str, groups, nextStrIndex, groupIndex, count + 1, memo);
   }
 
   memo[key] = total;
