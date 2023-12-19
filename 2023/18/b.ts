a0d6fd780d8d7c31fcc932acdfaf8d9f54a91358
Aivars Liepa <aivarsliepa1992@gmail.com>
2023-12-19 14:31:46 +0200

    2023 day 18 part 2

--- 2023/18/b.ts
+++ 2023/18/b.ts
@@ -15,66 +15,37 @@ const WEST = Object.freeze({ x: -1, y: 0 });
 let currentCoords: Coords = { x: 0, y: 0 };
 points.push(currentCoords);
 
-const input = readLinesSync("./test-input.txt");
-const distances: number[] = [];
-input.forEach(line => {
-  // const [direction, distance] = line.split(" ");
-  // const dist = Number(distance);
-  // const currDirection = direction === "R" ? EAST : direction === "L" ? WEST : direction === "U" ? NORTH : SOUTH;
+const input = readLinesSync("./input.txt");
+let boundryPoints = 0;
+
+input.forEach((line, i) => {
   const hex = line.split(" ")[2];
   const direction = hex[hex.length - 2];
   const distance = hex.substring(2, hex.length - 2);
 
   const dist = parseInt(distance, 16);
-  distances.push(dist);
   const currDirection = direction === "0" ? EAST : direction === "2" ? WEST : direction === "3" ? NORTH : SOUTH;
-
+  boundryPoints += dist;
   currentCoords = plusDirection(currentCoords, currDirection, dist);
   points.push(currentCoords);
-  console.log(direction, distance, dist);
 });
 
-console.log(findGCD(distances));
-console.log(calculateArea(), calculateArea2(), points.length);
+console.log(boundryPoints + calculateInteriorPoints());
 
-function calculateArea() {
-  // console.log(points);
-
-  let area = 0;
-  for (let i = 0; i < points.length; i++) {
-    let j = (i + 1) % points.length;
-    area += points[i].x * points[j].y;
-    area -= points[j].x * points[i].y;
-  }
-  return Math.abs(area / 2);
-}
-
-function gcd(a: number, b: number) {
-  if (b === 0) {
-    return a;
-  } else {
-    return gcd(b, a % b);
-  }
-}
-
-function findGCD(arr: number[]) {
-  let result = arr[0];
-  for (let i = 1; i < arr.length; i++) {
-    result = gcd(arr[i], result);
-  }
+function calculateInteriorPoints() {
+  const area = calculateArea(points);
+  const result = area - boundryPoints / 2 + 1;
   return result;
 }
 
-function calculateArea2(): number {
-  const vertices = points.slice().sort((a, b) => a.x - b.x);
-
+function calculateArea(vertices: Coords[]) {
   let area = 0;
-  for (let i = 0; i < vertices.length; i += 2) {
-    let j = (i + 1) % points.length;
-    area += Math.abs(vertices[i].y - vertices[j].y);
+  for (let i = 0; i < vertices.length; i++) {
+    let j = (i + 1) % vertices.length;
+    area += vertices[i].x * vertices[j].y;
+    area -= vertices[j].x * vertices[i].y;
   }
-
-  return area;
+  return Math.abs(area / 2);
 }
 
 function plusDirection(coords: Coords, direction: Coords, distance: number) {
